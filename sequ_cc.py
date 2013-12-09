0ce92b66e590042e8e408e1942aaa68c8033de00
CandaceBCox <candacebcox1@gmail.com>
2013-12-09 02:49:59 -0800

    CL3 implementation except for roman numerals

--- sequ_cc.py
+++ sequ_cc.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python
 #Candace Cox
 #main sequ function file
-#CL1
+#CL3 and CL4
 
+import string
 import sys
 import argparse
 
 #Global Variables
 print_array = []
+alpha_lower = string.ascii_lowercase
+alpha_upper = string.ascii_uppercase
 
 #---------Functions--------
 #sequence function
 def seq(x, y):
-	if x%1 == 0:
+	xnew = int(x)
+	ynew = int(y)
+	if xnew%1 == 0:
 		counter = int(x)
 	else:
-		counter = x
-	if y%1 == 0:
+		counter = xnew
+	if ynew%1 == 0:
 		y = int(y)
-	while counter <= y:
+	while counter <= ynew:
 		print_array.append(counter)
 		counter += 1
 
@@ -113,7 +118,76 @@ def padspaces():
 		tempint = print_array[int(i)-1]
 		newint = (str(i)).rjust((fill_len+3), ' ')
 		print_array[int(i)-1] = newint
-	print ('\n'.join(map(str,(print_array)))) 
+	print ('\n'.join(map(str,(print_array))))
+
+#format-word function
+def formatword():
+	print args.formatword
+	if args.formatword == ('arabic'):
+			newx = long(args.x)
+			newy = long(args.y)
+			if not args.z:
+				newz = 1
+			else:
+				newz = long(args.z[0]);
+			while newx <= newy:
+				print newx
+				newx += newz
+			sys.exit()
+
+	if args.formatword == ('floating'):
+			floatx = float(args.x)
+			floaty = float(args.y)
+			if not args.z:
+				floatz = 1
+			else:
+				floatz = float(args.z[0]);
+			while floatx <= floaty:
+				print floatx
+				floatx += floatz
+			sys.exit()
+
+	if args.formatword == ('alpha'):
+			lalphax = ord(args.x)
+			lalphay = ord(args.y)
+			if not args.z:
+				lcounter = 1
+			else:
+				lcounter = int(args.z[0])
+			for number in xrange(lalphax, lalphay+lcounter):
+				print chr(number)
+		
+			sys.exit()
+	if args.formatword == ('ALPHA'):
+			ualphax = ord(args.x)
+			ualphay = ord(args.y)
+			if not args.z:
+				ucounter = 1
+			else:
+				ucounter = int(args.z[0])
+			for number in xrange(ualphax, ualphay+ucounter):
+				print (chr(number).upper())
+			sys.exit()
+			
+	romanmap = [('i',1), ('iv',4), ('v',5), ('ix',9), ('x',10), ('xl',40), ('l',50), ('xc',90), ('c',100), ('cd',400), ('d',500), ('cm',900), ('m',1000)]
+	
+	if args.formatword == ('roman'):
+			print "i'm roman"
+	if args.formatword == ('ROMAN'):
+			print "i'm ROMAN"
+
+def int_test():
+	try:
+		int(args.x)
+		int(args.y)
+	except ValueError:
+		print "Please use integers or specify -F for format-word"
+	
+	if int(args.x) > int(args.y):
+		print "First integer should be less than second integer"
+		sys.exit
+	else:
+		seq(args.x, args.y)
 
 
 #------Main Driver------
@@ -125,42 +199,37 @@ parser.add_argument('-v', '--version', action='version', version='%(prog)s CL1')
 parser.add_argument('-f', '--format', action="store", dest="format")
 parser.add_argument('-w', '--equal-width', action="store_true", dest="equalwidth", default=False)
 parser.add_argument('-s', '--seperator', action="store", dest="seperator", nargs=1)
-parser.add_argument("x", type=float)
-parser.add_argument("y", type=float)
+parser.add_argument("x")
+parser.add_argument("y")
+parser.add_argument("z", nargs='*')
 parser.add_argument('-W', '--words', action="store_true", dest="words", default=False)
 parser.add_argument('-p', '--pad', action="store", dest="pad")
 parser.add_argument('-P', '--pad-spaces', action="store_true", dest="padspaces", default=False)
+parser.add_argument('-F', '--format-word', dest="formatword", action="store", choices=['arabic', 'floating', 'alpha', 'ALPHA', 'roman', 'ROMAN'])
 
 args = parser.parse_args()
 
-
 #check user input for errors
-if args.x != 0 and args.y != 0:
-	try:
-		val = args.x
-		val2 = args.y
-	except ValueError:
-		print "Please use integers or decimals only"
-	if args.x < args.y:
-		seq(args.x, args.y)
-		if args.format:
-			format()
-		elif args.equalwidth:
-			equalwidth()
-		if args.seperator:
-			seperator()
-		elif args.words:
-			words()
-		elif args.pad:
-			pad()
-		elif args.padspaces:
-			padspaces()
-		else:
-			printseq(print_array)
-	else:
-		print "First integer should be less than second integer"
-		sys.exit
+if args.formatword:
+	formatword()
+if args.format:
+	int_test()
+	format()
+elif args.equalwidth:
+	int_test()
+	equalwidth()
+if args.seperator:
+	int_test()
+	seperator()
+elif args.words:
+	int_test()
+	words()
+elif args.pad:
+	int_test()
+	pad()
+elif args.padspaces:
+	int_test()
+	padspaces()
 else:
-	print "Please use two arguments"
-	sys.exit
-
+	seq(args.x, args.y)
+	printseq(print_array)
